bb9573abfea6bfe7e443a8df2f52a372416bfa24
doggersen <martin.mbj.johansen@gmail.com>
2021-04-04 19:19:43 +0200

    Add project files.

---
+++ BajerOgBillardTest/BajerOgBillardTest/Player.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BajerOgBillardTest
+{
+    class Player
+    {
+        public string Name { get; set; }
+        public int Points { get; set; }
+        
+        public Player(string name, int points)
+        {
+            Name = name;
+            Points = points;
+
+        }
+
+
+        //private double _name;
+        //public double Name
+        //{
+        //    get { return _name; }
+        //    set
+        //    {
+        //        _name = value;
+        //        OnPropertyChanged();
+        //    }
+        //}
+
+        //private double _points;
+        //public double Points
+        //{
+        //    get { return _points; }
+        //    set
+        //    {
+        //        _points = value;
+        //        OnPropertyChanged();
+        //    }
+        //}
+
+
+
+    }
+}
---
+++ BajerOgBillardTest/BajerOgBillardTest/PlayerListViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace BajerOgBillardTest
+{
+    class PlayerListViewModel
+    {
+        public ObservableCollection<Player> Players { get; set; }
+
+
+        public PlayerListViewModel()
+        {
+            Players = new ObservableCollection<Player>();
+
+            Players.Add(new Player("Jimmy", 60));
+            Players.Add(new Player("Cherryl", 60));
+            Players.Add(new Player("Martin", 60));
+
+        }
+
+
+    }
+}
