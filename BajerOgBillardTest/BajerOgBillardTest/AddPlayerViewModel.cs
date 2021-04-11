58076d805325240d3af6173409066e42b625878f
doggersen <martin.mbj.johansen@gmail.com>
2021-04-11 22:35:39 +0200

    start lidt forfra, prøver at fjerne messagingCenter og lade begge xaml-sider linke til samme listviewmodel

--- BajerOgBillardTest/BajerOgBillardTest/AddPlayerViewModel.cs
+++ BajerOgBillardTest/BajerOgBillardTest/AddPlayerViewModel.cs
@@ -4,28 +4,69 @@ using Xamarin.Forms;
 
 namespace BajerOgBillardTest
 {
-    internal class PlayerListViewModel
+    internal class AddPlayerViewModel
     {
+        public Player Player { get; set; }
+
+        //public AddPlayerViewModel()
+        //{
+        //    Player = new Player();
+        //}
+
         public ICommand AddPlayerCommand => new Command(AddPlayer);
         public ICommand DeletePlayerCommand => new Command(DeletePlayer);
         public ICommand UpdatePlayerCommand => new Command(UpdatePlayer);
 
         public ObservableCollection<Player> Players { get; set; }
+        //private ObservableCollection<Player> _players { get; set; }
+        //public ObservableCollection<Player> Players
+        //{
+        //    get { return _players; }
+        //    set
+        //    {
+        //        _players = value;
+        //        OnPropertyChanged();
+        //    }
+        //}
 
         //since we deal with the Player objects, we need this temporary variable to store user-input.
         //so later, when we need to add player objects to the list, we can add "InputName" (that contains the user input)
+
         public string InputName { get; set; }
+
+        //public string Name { get; set; }
+
+        //private string _inputName;
+        //public string InputName
+        //{
+        //    get { return _inputName; }
+        //    set
+        //    {
+        //        _inputName = value;
+        //        OnPropertyChanged();
+        //    }
+        //}
 
         public Player SelectedPlayer { get; set; }
 
-        public PlayerListViewModel()
+        public AddPlayerViewModel()
         {
+            //Player = new Player();
+
             Players = new ObservableCollection<Player>();
 
             //The player class consists of Name and Points, i only write name here, because the points have a default value of 60
             Players.Add(new Player("Jimmy"));
             Players.Add(new Player("Cherryl"));
             Players.Add(new Player("Martin"));
+
+            MessagingCenter.Subscribe<AddPlayerPage, Player>(this, "AddPlayer",
+               (page, player) =>
+               {
+                   Players.Add(player);
+               }
+
+               );
         }
 
         public void AddPlayer()
